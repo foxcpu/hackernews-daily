cca633c66f62527aff851c709955dc5b6665ffe9
huhuiping <huhuiping@jiuqi.com.cn>
2024-04-27 18:52:34 +0800

    test

--- test/getHeadlines.js
+++ test/getHeadlines.js
@@ -3,6 +3,6 @@ const getHeadlines = require('../utils/getHeadlines');
 
 test('getHeadlines', async () => {
   const headlines = await getHeadlines(new Date('2020-09-06'));
-  // test
+  // test  test  test
   console.log(headlines)
 });
